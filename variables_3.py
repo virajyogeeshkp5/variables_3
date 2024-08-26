603763260a635ddd4818b527d06e150f81e15461
YOGEESH K P <yogeesh793@gmail.com>
2024-08-26 17:15:13 +0530

    Add files via upload

---
+++ variables_3.py
@@ -0,0 +1,21 @@
+# variables
+# Integer variable
+my_integer = 10
+
+# String variable
+my_string = "Hello,there!"
+
+# Boolean variable
+is_true = True
+
+# List variable
+my_list = [1,2,3,4,5]
+
+# Float variable
+my_float = 3.14
+
+# Printing the variable
+print("Integer:",my_integer)
+print("String:",my_string)
+print("Boolean:",is_true)
+print("Float:",my_float)
